abe0c979cdd9f500cef46398c96c21aa8a2775c4
Ivan <ivantellez04@iesamachado.org>
2024-01-22 20:13:50 +0100

    Lo siento joseluis  mi cabeza está💀🤯

---
+++ parcticas_Amaro/ej_class_amaro.js
@@ -0,0 +1,15 @@
+
+var ElementList = [""]
+
+
+function add() {
+    var elemento = document.getElementById("introducido").value;
+    ElementList.push(elemento);
+    document.getElementById("escribir").innerHTML = ElementList;
+}
+
+function remove(){}
+function dec(){}
+function crec(){}
+function search(){}
+
---
+++ parcticas_Amaro/practica_array_amaro.js
@@ -0,0 +1,14 @@
+
+ var Array = ["amaro", "antonio perro","que pasa hackers" ]
+
+ function escribir()
+ 
+ {
+    for (i=0; i <= 2; i++)
+ {
+    document.write(Array[i] + "<br>")
+ }
+
+ }
+
+ 
